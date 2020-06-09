0a447ce2d21c6a6d00475681eb886d2c4409dc01
dev <ash.gabriel+dev@gmail.com>
2020-06-09 16:10:09 +0200

    deproject name url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject gabrielash/defnw "0.1.0"
-  :description "depnw a defn substitute macro with boundary case moved to the pre-post map"
-  :url "http://github.com/gabrielash"
+  :description "depnw is a defn substitute macro with boundary case moved to the pre-post map"
+  :url "http://github.com/gabrielash/defnw"
 
   :license {:name "GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
